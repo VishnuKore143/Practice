aa0dde6736f91ab46d304eaaf3696f5a7a64956f
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-19 11:58:42 +0530

    Add files via upload

---
+++ LogicalOperator.java
@@ -0,0 +1,18 @@
+package Practices;
+
+public class LogicalOperator {
+public static void main(String[]args) {
+	
+	int a = 5;
+	int b = 6;
+	int c  = a&b;
+	System.out.println(c);
+	
+	// Logical OR Operator
+	int x = 5;
+	int y = 6;
+	int z = x|y;
+	System.out.println(z);
+	
+}
+}
