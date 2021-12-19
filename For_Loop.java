ef753cade81c60f43b672f23b5ed96554344940d
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-20 00:15:37 +0530

    Add files via upload

---
+++ For_Loop.java
@@ -0,0 +1,14 @@
+package Practices;
+import java.util.Scanner;
+public class For_Loop {
+public static void main(String[]args) {
+Scanner sc = new Scanner(System.in);
+	
+	for(int i = 0; i<10; i++) {
+		System.out.println(i);
+		
+		
+	}
+	
+}
+}
