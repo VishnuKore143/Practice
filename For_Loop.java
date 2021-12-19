d4912655eb4467c13f404b4cd23c46787fe5a57a
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-20 00:16:16 +0530

    Update For_Loop.java

--- For_Loop.java
+++ For_Loop.java
@@ -1,14 +1,12 @@
 package Practices;
-import java.util.Scanner;
+
 public class For_Loop {
 public static void main(String[]args) {
-Scanner sc = new Scanner(System.in);
 	
 	for(int i = 0; i<10; i++) {
 		System.out.println(i);
 		
 		
-	}
-	
-}
+    }
+  }
 }
