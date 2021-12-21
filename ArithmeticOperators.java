17b18d9db82be734841f1ca9d23c85fdd06f81a0
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-22 00:14:08 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,29 @@
+package Practices;
+
+public class ArithmeticOperators {
+	public static void main(String[]args) {
+		
+		int a = 9;
+		int b = 1;
+		System.out.println("The + is the arithmetic operator 9+1 = "+a+b);
+		
+		int x = 10;
+		int y = 8;
+		System.out.println("The - is the arithmetic operator 10-8 = "+(x-y));
+		
+		int k = 9;
+		int j = 1;
+		System.out.println("The * is the arithmetic operator 9*1 = "+k*j);
+		
+		int i = 9;
+		int p = 1;
+		System.out.println("The / is the arithmetic operator 9/1 = "+i/p);
+		
+		int c = 9;
+		int d = 1;
+		System.out.println("The % is the arithmetic operator 9%1 = "+c%d);
+		
+		
+	}
+
+}
