8300df075a609fcd6a49c9107a5f2171c9cb543b
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-25 04:07:36 +0530

    Add files via upload

---
+++ NestedForLoop.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SCA_Leactures;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vishnu Kore
+ */
+public class NestedForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 1; i <=2; i++) {
+            for (int j = 1; j <=3; j++) {
+               
+                System.out.println(j);
+                
+            }
+        }
+        
+    }
+    
+}
