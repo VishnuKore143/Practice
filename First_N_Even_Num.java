449e8a8e2875c64f2b0fdc0fd34e2822ba33ca63
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-25 23:47:39 +0530

    Add files via upload

---
+++ First_N_Even_Num.java
@@ -0,0 +1,15 @@
+package CWH;
+
+public class First_N_Even_Num {
+    public static void main(String[] args) {
+
+        int sum=0;
+        int n=3;
+        for (int i=0; i<n; i++){
+            sum=sum+(2*i);
+        }
+        System.out.print(sum);
+
+
+    }
+}
