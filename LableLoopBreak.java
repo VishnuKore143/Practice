f46006941197b0bbf7554bcefbc0eeb9d386852c
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-28 08:44:52 +0530

    Add files via upload

---
+++ LableLoopBreak.java
@@ -0,0 +1,12 @@
+class LableLoopBreak{
+	public static void main(String[]args){
+		Bhopal:
+		for(int i=1; i<=3; i++){
+			for(int j=5; j<=15; j++){
+				System.out.println(j);
+					if(j%6==0)
+					Break Bhopal;
+					}
+				}
+			}	
+		}
