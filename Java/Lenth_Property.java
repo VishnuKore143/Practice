2c6be20d055e77163dfa27fcfc5d64cd09e5a626
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-01-02 00:06:25 +0530

    Add files via upload

---
+++ Java/Lenth_Property.java
@@ -0,0 +1,31 @@
+package SCA;
+
+import java.util.Scanner;
+
+public class Lenth_Property {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr ={10,20,30,40,50,60};
+        //Normal Array Printing
+        System.out.println("This is Normal Array Printing");
+        System.out.println("Size of array is :"+arr.length);
+        for (int i=0; i<arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+        System.out.println();
+        System.out.println("================================================");
+        System.out.println("================================================");
+        System.out.println();
+        System.out.println("This is Reverse Array Printing");
+
+        //Reverse Array printing
+        System.out.println("Array size is :"+arr.length);
+        for (int i=arr.length -1; i>=0; i--){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
