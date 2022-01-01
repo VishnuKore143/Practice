43c583c1364c6dccb064dace068e9d90b9e50a44
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-01-02 00:48:34 +0530

    Add files via upload

---
+++ Java/ForEachLoop.java
@@ -0,0 +1,29 @@
+package SCA;
+
+import javax.print.attribute.EnumSyntax;
+import java.util.Scanner;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+/*        Syntax:
+        for(data-type; variable-name; Array-ref-Name)
+        {
+        //Loop Body;
+        }
+*/
+
+
+    //Code
+
+        int []arr = {10,20,30,40,50};
+        for (int x: arr){
+            System.out.println(x);
+        }
+
+
+
+
+    }
+}
