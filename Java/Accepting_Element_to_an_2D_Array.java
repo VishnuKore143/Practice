0709f361ee216f98b0f92e03de9be2715bb7e066
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-01-03 00:03:41 +0530

    Add files via upload

---
+++ Java/Accepting_Element_to_an_2D_Array.java
@@ -0,0 +1,24 @@
+package SCA;
+
+import java.util.Scanner;
+
+public class Accepting_Element_to_an_2D_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Syntax :
+        /*
+        <array-reference> [row index] [column index] = value;
+                                or
+        variable-name = <array-reference>[row index][column index];
+         */
+
+        //Code Example
+
+        int [][]arr = new int[3][4];
+        arr[1][0]=15;
+        int x = arr[0][0];
+
+
+    }
+}
